5c5c46a60b5f3d5d4466498fd6a5c761ae1f1cac
Mykhaylo Gavrylyuk <mykhaylo.gavrylyuk@gmail.com>
2014-06-13 01:44:31 -0700

    rv.js (Reactive Value) => dv.js (Dynamic Value)

---
+++ dv-jquery.js
@@ -0,0 +1,14 @@
+$.fn.dvHtml = function (dv) {
+  var el = this;
+  dv.onchange(function (v) { el.html(v); });
+  el.html(dv.value);
+  return el;
+}
+
+$.fn.dvVal = function (dv) {
+  var el = this;
+  el.on("input", function () { dv.value = el.val(); });
+  dv.onchange(function (v) { el.val(v); });
+  el.val(dv.value);
+  return el;
+};
--- dv.js
+++ dv.js
@@ -1,4 +1,4 @@
-function rv(value) {
+function dv(value) {
   if (arguments.callee !== this.constructor) {
     var i, arr = [];
     for (i = 0; i < arguments.length; i++) { arr.push('arguments[' + i + ']'); }
@@ -25,11 +25,11 @@ function rv(value) {
   }
 }
 
-rv.prototype.__defineGetter__('value', function () {
+dv.prototype.__defineGetter__('value', function () {
   return this._value;
 });
 
-rv.prototype.__defineSetter__('value', function (newValue) {
+dv.prototype.__defineSetter__('value', function (newValue) {
   var oldValue = this._value;
   if (this._value !== newValue) {
     this._value = newValue;
@@ -38,7 +38,7 @@ rv.prototype.__defineSetter__('value', function (newValue) {
   }
 });
 
-rv.prototype._triggerChange = function (newValue, oldValue) {
+dv.prototype._triggerChange = function (newValue, oldValue) {
   if (this._changeHandlers) {
     for (var i = 0; i < this._changeHandlers.length; i++ ) {
       this._changeHandlers[i](newValue, oldValue);
@@ -46,7 +46,7 @@ rv.prototype._triggerChange = function (newValue, oldValue) {
   }
 };
 
-rv.prototype._propagateChange = function () {
+dv.prototype._propagateChange = function () {
   if (this._deps) {
     for (var i = 0; i < this._deps.length; i++) {
       this._deps[i]._calculateValue();
@@ -55,7 +55,7 @@ rv.prototype._propagateChange = function () {
   }
 };
 
-rv.prototype._calculateValue = function () {
+dv.prototype._calculateValue = function () {
   if (!this._args || !this._fn) return;
   var newValue = this._fn.apply(null, this._args),
     oldValue = this._value;
@@ -65,35 +65,35 @@ rv.prototype._calculateValue = function () {
   }
 };
 
-rv.prototype.onchange = function (handlerFn) {
+dv.prototype.onchange = function (handlerFn) {
   if (!this._changeHandlers) this._changeHandlers = [];
   this._changeHandlers.push(handlerFn);
 };
 
-rv.prototype.cleanup = function () {
+dv.prototype.cleanup = function () {
   delete this._changeHandlers;
 };
 
-rv.prototype.link = function (rvOther) {
-  if (!(rvOther instanceof rv)) throw "";
-  this._linkedTo = rvOther;
+dv.prototype.link = function (dvOther) {
+  if (!(dvOther instanceof dv)) throw "";
+  this._linkedTo = dvOther;
   if (!this._linkedTo._deps) { this._linkedTo._deps = []; }
   this._linkedTo._deps.push(this);
   this._value = this._linkedTo._value;
 };
 
-rv.prototype.unlink = function () {
+dv.prototype.unlink = function () {
   this._linkedTo._deps.splice(this._linkedTo._deps.indexOf(this), 1);
   delete this._linkedTo;
 };
 
-rv.lift = function (fn) {
+dv.lift = function (fn) {
   var cls = this;
   return function () {
     return cls(fn, Array.prototype.slice.call(arguments));
   };
 };
 
-rv.prototype.map = function (mapFn) {
-  return rv.lift(mapFn)(this);
+dv.prototype.map = function (mapFn) {
+  return dv.lift(mapFn)(this);
 };
