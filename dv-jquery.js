c54040efbb43bde3250814b5a06737a195540a09
Mykhaylo Gavrylyuk <mykhaylo.gavrylyuk@gmail.com>
2014-07-06 21:56:21 -0700

    Linted dv-jquery.js

--- dv-jquery.js
+++ dv-jquery.js
@@ -1,20 +1,20 @@
 (function ($) {
-    
-    if (!$) return;
-    
-    $.fn.dvHtml = function (dv) {
-      var el = this;
-      dv.onchange(function (v) { el.html(v); });
-      el.html(dv.value);
-      return el;
-    }
-    
-    $.fn.dvVal = function (dv) {
-      var el = this;
-      el.on("input", function () { dv.value = el.val(); });
-      dv.onchange(function (v) { el.val(v); });
-      el.val(dv.value);
-      return el;
-    };
-    
+
+  if (!$) return;
+
+  $.fn.dvHtml = function (dv) {
+    var el = this;
+    dv.onchange(function (v) { el.html(v); });
+    el.html(dv.value);
+    return el;
+  }
+
+  $.fn.dvVal = function (dv) {
+    var el = this;
+    el.on("input", function () { dv.value = el.val(); });
+    dv.onchange(function (v) { el.val(v); });
+    el.val(dv.value);
+    return el;
+  };
+
 })(jQuery);
